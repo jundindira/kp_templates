e841e9b2ccd7da69abf191de31fac9434524cddc
jundindira <jundindira@it.student.pens.ac.id>
2024-12-02 21:35:27 +0700

    menambahkan route hello dan dashboard

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route::get('/dashboard', [DashboardController::class, 'index']);
+
+Route::get('/hello', function () {
+    return 'Hello world';
+});
